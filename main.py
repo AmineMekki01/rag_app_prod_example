dae0f8a239cc69676b14fe6995d0487b829aba31
Amine MEKKI <amine.mekki@mines-ales.org>
2023-11-03 21:11:36 +0100

    adding APIs to the main flask api file

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.core.logs import logger
+
+from src.app.chat.api import router as chat_router
 from src.app.core.api import router as core_router
+
 from src.app import version
 
-
 app = FastAPI(version=version)
-app.include_router(core_router)
-logger.info("Hey there!")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(core_router)
+app.include_router(chat_router)
+
+logger.info("App is ready!")
