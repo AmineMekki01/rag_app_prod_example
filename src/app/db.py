5af31f5a2f05cb7aa63122750660737f256acb6c
Amine MEKKI <amine.mekki@mines-ales.org>
2023-11-03 21:10:14 +0100

    pugsql connection file

---
+++ src/app/db.py
@@ -0,0 +1,5 @@
+import pugsql
+from src.app.settings import settings
+
+messages_queries = pugsql.module("db/queries/messages")
+messages_connection = messages_queries.connect(settings.DATABASE_URL)
